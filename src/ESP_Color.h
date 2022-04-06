8f5535d4edd02d55be3bef8055b99b084ea61a55
Luis Llamas <luisllamas@hotmail.com>
2022-04-06 16:19:52 +0200

    V1.0.1
    , Added library.properties for Arduino IDE compatibility

--- src/ESP_Color.h
+++ src/ESP_Color.h
@@ -13,7 +13,7 @@ You should have received a copy of the GNU Affero General Public License along w
 #ifndef _ESP_COLOR_H_
 #define _ESP_COLOR_H_
 
-#define _ESP_COLOR_VERSION_ "1.0.0"
+#define _ESP_COLOR_VERSION_ "1.0.1"
 
 #include <cstddef>
 #include <cstdint>
